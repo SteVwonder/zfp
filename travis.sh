b5e557c8feab4a234c49a84855dbb07b7ee0d9c0
Stephen Herbein <stephen272@gmail.com>
2018-12-23 10:42:29 -0500

    travis: add BUILD_PYTHON to build matrix

--- travis.sh
+++ travis.sh
@@ -4,24 +4,26 @@ set -e
 mkdir build
 cd build
 
+CMAKE_FLAGS="-DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_PYTHON=${BUILD_PYTHON:-off}"
+
 if [ -n "${COVERAGE}" ]; then
 
   # build
-  cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DZFP_WITH_OPENMP=ON -DCMAKE_C_FLAGS=-coverage -DCMAKE_CXX_FLAGS=-coverage;
+  cmake .. ${CMAKE_FLAGS} -DBUILD_CFP=ON -DZFP_WITH_OPENMP=ON -DCMAKE_C_FLAGS=-coverage -DCMAKE_CXX_FLAGS=-coverage;
   cmake --build . ;
   ctest -V -C "Debug";
 
 else
 
   # build/test without OpenMP, with CFP
-  cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DZFP_WITH_OPENMP=OFF -DBUILD_CFP=ON;
+  cmake .. ${CMAKE_FLAGS} -DZFP_WITH_OPENMP=OFF -DBUILD_CFP=ON;
   cmake --build . ;
   ctest -V -C "Debug";
 
   rm -rf ./* ;
 
   # build/test with OpenMP, with CFP custom namespace
-  cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DCFP_NAMESPACE=cfp2;
+  cmake .. ${CMAKE_FLAGS} -DBUILD_CFP=ON -DCFP_NAMESPACE=cfp2;
   cmake --build . ;
   ctest -V -C "Debug";
 
